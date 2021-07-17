9f2c33b32fe8bd795df46fa6045a535b16f428c1
John <jbartlettdesign@gmail.com>
2021-07-16 19:12:15 -0500

    setup login/signup css and react

--- client/src/App.js
+++ client/src/App.js
@@ -4,16 +4,18 @@ import Home from './components/Home';
 import User from './components/User';
 import DogWalker from './components/DogWalker';
 import Login from './components/Login';
-import SignIn from './components/SignIn';
-
+import SignUp from './components/SignUp';
+import Navbar from './components/Navbar';
+import {Route, Link} from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-    <Home />
-    <DogWalker />
-    <User />
-    <Login />
-    <SignIn />
+    <Navbar />
+    <Route exact path= "/" component = {Home}/>
+    <Route exact path= "/User" component = {User}/>
+    <Route exact path= "/DogWalker" component = {DogWalker}/>
+    <Route exact path= "/Login" component = {Login}/>
+    <Route exact path= "/SignUp" component = {SignUp}/>
     </div>
   );
 }
--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -1,11 +1,54 @@
 import React from 'react';
- function Login(){
+import { Route } from 'react-router-dom'
+
+
+function Login(){
+    
      return(
-         <div>
-             <h1>
-                 Welcome to login!
-             </h1>
-         </div>
+        <section className = 'totalLayout'>
+        <div className = 'leftSwitchedLayout'>
+            <div className = 'blueOutline'>
+               <h1>
+               Login to view your details and find dog walkers in your area.               </h1>
+            </div>
+        </div>
+
+        <div className = 'rightSwitchedLayout'>
+
+            <div className= 'alignForm'>
+
+           
+            <Route render={({ history}) => (
+           <h1
+               type='button'
+               onClick={() => { history.push('/SignUp') }}
+           >
+               Sign up Instead
+           </h1>
+       
+           )} />
+           
+               <form className = "alignSwitchedForm">
+                   <label>Username
+
+                   <input type="text" name="name" />
+
+                  </label>
+                   <label>Password
+                   
+                   <input type="text" name="password" />
+
+                   </label>
+               </form>
+           </div>
+           <div className = "buttonDiv"><button className = "yellowSwitchedButton" type="submit">Login</button></div>
+           
+        </div>
+        
+        <div className= "bottomSwitchedBlue">
+
+        </div>
+        </section>
      )
  };
 
