f14ea415da33f2ce3a4fdfcd91cc96a7917cab69
japankid-code <japankid.jake@gmail.com>
2021-07-26 19:41:18 -0500

    add dependencies and apolloProvider to App

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
-import Home from './components/Home';
-import User from './components/User';
-import DogWalker from './components/DogWalker';
-import Login from './components/Login';
-import SignUp from './components/SignUp';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import {Route, Link} from 'react-router-dom';
+import "./App.css";
+import Home from "./components/Home";
+import User from "./components/User";
+import DogWalker from "./components/DogWalker";
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
+import Footer from "./components/Footer";
+import { Route } from "react-router-dom";
+
+// add apollo client to the App
+import { setContext } from "@apollo/client/link/context";
+
+import {
+  ApolloProvider, // provides data to componenents
+  ApolloClient, // constructor, initializes connection to server
+  InMemoryCache, // caches API responses for efficiency
+  createHttpLink, // controls how client makes requests
+} from "@apollo/client";
+
+const httpLink = createHttpLink({
+  uri: "/graphql", // links to GraphQL server
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  // set the http headers of every request to include the token!!
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  // tie the auth and http links together!!
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <div className="App">
-    
-    <Route exact path= "/" component = {Home}/>
-    <Route exact path= "/User" component = {User}/>
-    <Route exact path= "/DogWalker" component = {DogWalker}/>
-    <Route exact path= "/Login" component = {Login}/>
-    <Route exact path= "/SignUp" component = {SignUp}/>
-    <Footer/>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <Route exact path="/" component={Home} />
+        <Route exact path="/User" component={User} />
+        <Route exact path="/DogWalker" component={DogWalker} />
+        <Route exact path="/Login" component={Login} />
+        <Route exact path="/SignUp" component={SignUp} />
+        <Footer />
+      </div>
+    </ApolloProvider>
   );
 }
-/*<Navbar />*/
+
 export default App;
