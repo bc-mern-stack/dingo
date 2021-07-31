62eb0449e8ee44a92fb3ee46fffff93ae62fabc0
John <jbartlettdesign@gmail.com>
2021-07-31 11:43:30 -0500

    location updates

--- client/src/components/User/index.js
+++ client/src/components/User/index.js
@@ -3,114 +3,171 @@ import { Route } from 'react-router-dom';
 import Calendar from 'react-calendar'
 import dogHouse from '../../assets/dogHouse.png'
 import { useQuery, gql, useMutation } from '@apollo/client';
-import { QUERY_USER, QUERY_ME } from '../../utils/queries';
+import { QUERY_USERS, QUERY_ME } from '../../utils/queries';
 import { ADD_DOGGO } from '../../utils/mutations';
 import Auth from '../../utils/auth';
-import { useParams } from 'react-router-dom';
+
 
 function User() {
-    const {  data,error } = useQuery(QUERY_ME);
-    console.log(data);
-    console.log(error);
-    {Auth.loggedIn && data ? (
-  <div>
-    
-      username={data.me.username}
-      
-    
-  </div>
-) : null}
-     if (Auth.loggedIn())
-     {
-         console.log('logged in')
-         
-     }
-    {/*   const { loading, error, data } = useQuery(QUERY_USER, {
-  variables: { username: "Darby.Orn" }
-});
-const user = data?.user || [];*/}
-    /*console.log(user);*/
-   /*const { username: userParam } = useParams();
 
-  const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
+    /***************************************************************************************/
+        const {  loading, data, error } = useQuery(QUERY_ME);
     
-      variables: { username: userParam }
-  });
+        const me = data?.me || {};
 
-    const user = data?.me || data?.user || {};
-   
-    if (
-    Auth.loggedIn() &&
-    Auth.getProfile().data.username === userParam
-    ) {
-        console.log("you are logged in")
-    //return <Redirect to="/profile" />;
-  }*/
-    /*console.log(user);*/
+        const { data: userData } = useQuery(QUERY_USERS);
     
-    const [addDoggo, { addDogError }] = useMutation(ADD_DOGGO);
+       // console.log(userData)
     
-    const handleFormSubmit = async event => {
-    event.preventDefault();
-
-  // use try/catch instead of promises to handle errors
-  try {
-    // execute addUser mutation and pass in variable data from form
-      const { data } = await addDoggo({
-          variables: {
-              username:"Darby.Orn",
-              doggos: {
-                    name: "Mars",
-                    size: 50,
-                    age: 5,
-                    breed: "Husky",
-                    behavior: "Gentle",
-                    temperament: "Calm",
-                    picture: "placeholder",
-      }
-    }
-    });
-    console.log(data);
-  } catch (e) {
-      console.error(e);
-      console.log("error")
-  }
-};
-
+    /*****************************************************************************************/
     const [value, onChange] = useState(new Date());
+
+    /************************switch between dog walkers and current*********************************/
     const [isOpened, setIsOpened] = useState(false);
-    
+
+    function newDog() {
+        createNewDog(true);
+        }
+
         function toggle() {
             setIsOpened(true);
           }
           function toggleOff() {
             setIsOpened(false);
           }
+      
+    
+
+   
+    /************************add new dog*********************************/
+    const [addNewDog, createNewDog] = useState(false);
+    const [dogFormState, setDogState] = useState({
+        name: "",
+        size: "",
+        age: "",
+        breed: "",
+        behavior: "",
+        temperament: "",
+        picture: ""
+      });
+    
+    /*************************location search*********************************/
+    const [newEntry, setNewentry] = useState(false);
+    
+      const [addressSearch, newAddressSearch] = useState({
+            city:""
+      });
+    const [totalSearch, setTotalSearch] = useState(
+        []
+    );
+    
+    const handleSearchAddress = (event) => {
+        const { name, value } = event.target;
+        console.log(name, value)
+        newAddressSearch({
+            ...addressSearch,
+            [name]: value
+        });
+        }
+    const handleNewSearchForWalkers = async event => {
+        event.preventDefault();
+        const peopleInMyCity = []
+        const myCity = data.me.address.city;
+        console.log(addressSearch.city);
+        userData.users.forEach(element => {
+        
+           
+            if (newEntry === true) {
+                    setNewentry(false)
+            }
+            
+            if (element.address.city === addressSearch.city
+                && element.username != data.me.username) {
+                totalSearch.push(element.username);
+                setNewentry(true)
+            
+           }
+
+            
+        });
+    }
+    /***********************************************************/
+
+    const dogData = { ...dogFormState };
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        if (name === "picture") {
+            console.log(event.target.files[0])
+
+            const formData = new FormData();
+            formData.append('file', event.target.files[0]);
+            formData.append('upload_preset', 'l9bmcax8');
+            fetch('https://api.cloudinary.com/v1_1/kingsley-illustration/upload', {
+                method: 'POST',
+                body: formData
+            }).then(res => res.json())
+                .then(res => 
+                    //console.log(res.secure_url)
+                    setDogState({
+                      ...dogFormState, 
+                        picture: res.secure_url.toString(),
+                
+            })
+            );
+            
+        } else {
+            setDogState({
+                ...dogFormState,
+                [name]: value,
+            });
+        }
+        console.log(dogFormState);
+  };
+
+    const [addDoggo, { addDogError }] = useMutation(ADD_DOGGO, {
+        update(cache, { data: { addDoggo } }) {
+            // read what's currently in the cache
+            const { dogData } = cache.readQuery({ query: QUERY_ME });
+
+            // prepend the newest thought to the front of the array
+            cache.writeQuery({
+                query: QUERY_ME,
+                data: { doggos: addDoggo }
+            });
+            window.location.reload();
+
+        }
+    });
+    
+                const handleFormSubmit = async event => {
+                event.preventDefault();
+                        dogData.size = parseInt(dogData.size);
+                        dogData.age = parseInt(dogData.age);
+                        console.log(dogData)
+            // use try/catch instead of promises to handle errors
+            try {
+                // execute addUser mutation and pass in variable data from form
+                await addDoggo({
+                    variables: {
+                        ...dogData
+                            }
+                
+                });
+                if (data) {
+                    
+                    console.log(data)
+                }
+                } catch (e) {
+                console.error(e);
+                }
+            
+                };
 
      return(
          
          <div>
-
-             {/*<div>
-                    <p className="m-0">
-                        Character Count: 0/280
-                    </p>
-                    <form onSubmit = {handleFormSubmit} className="flex-row justify-center justify-space-between-md align-stretch">
-                        <textarea
-                        placeholder="Here's a new thought..."
-                        className="form-input col-12 col-md-9"
-                        ></textarea>
-                        <button className="btn col-12 col-md-3" type="submit">
-                        Submit
-                     </button>
-                     {error && <div>Sign up failed</div>}
-
-                    </form>
-                    </div>
-                    */}
-{/*
+             
              <div className="allUserElements">
-                 
                 <div className = "userNav">
                     <h1>
                         Owner Profile
@@ -138,7 +195,7 @@ const user = data?.user || [];*/}
                     </div>
                 </div>
 
-                 <h1 className="blackBar">{user.username}</h1>
+                 <h1 className="blackBar">{me.username}</h1>
 
                  <div className = "leftAndRight">
                     
@@ -150,15 +207,16 @@ const user = data?.user || [];*/}
                                { loading ? (<div>...Loading</div>
                          ) : (
                                  <div className="text contact">
-                                     <h2>Address</h2>
-                                     <h4>{user.address.street}</h4>
+                                         <h2>Address</h2>
+                                         
+                                     <h4>{me.address.street}</h4>
                                 
-                                     <h4>{user.address.city} {user.address.state}</h4>
+                                     <h4>{me.address.city} {me.address.state}</h4>
 
                                      <div className="contact">
                                          <h2>Contact</h2>
-                                         <h4>{user.email}</h4>
-                                         <h4>{user.address.phone_number}</h4>
+                                         <h4>{me.email}</h4>
+                                         <h4>{me.address.phone_number}</h4>
                                      </div>
                                  </div> )}
                              
@@ -171,7 +229,7 @@ const user = data?.user || [];*/}
                                      {loading ? (<li>...Loading</li>
                                  ) : (
                                            
-                                     user.favorites.map(favorites => (
+                                     me.favorites.map(favorites => (
                                         
                                              <li key={favorites._id}>{ favorites.username}</li>
                                              
@@ -196,15 +254,17 @@ const user = data?.user || [];*/}
                      </div>
                         <div>
                                 <ul className = "dogWalkersRight scroll text">
-                                     <li>Jane Doe</li>
-                                     <li>John Smith</li>
-                                     <li>Steve Smith</li>
-                                     <li>Jack White</li>
-                                     <li>Jane Smith</li>
-                                     <li>John White</li> 
+                                     {newEntry ?
+                                         totalSearch.map(person => (
+                                        
+                                             <li key={person.toString()}>{person}</li>
+                                             
+                                        
+                                         )):<div></div>
+                                        }
                                  </ul>
-                     <form>
-                         <input className = "newLocationSearchInput" placeholder = "New Search" type="text" name="name" />
+                     <form onSubmit = {handleNewSearchForWalkers}>
+                         <input name="city" autoComplete="" value={addressSearch.name} onChange= {handleSearchAddress} className = "newLocationSearchInput" placeholder = "New Search" type="text" />
                          <button className = "newLocationSearchButton" type="submit"></button>
                         
                      </form>
@@ -226,15 +286,87 @@ const user = data?.user || [];*/}
                  <div>
                      <div className="completeTitle">
                           {data &&
-                         <h4 className="dogTitle" >Your Dogs: { user.doggos.length}</h4>
+                         <h4 className="dogTitle" >Your Dogs: { me.doggos.length}</h4>
                          }
-                         <img className = "add" src={dogHouse} alt="dog house"></img>
+                         <img onClick = {newDog} className = "add" src={dogHouse} alt="add buton"></img>
                      </div>
-             <div className = "yourDogs">
+                     <div className="yourDogs">
+                         {addNewDog ?
+                     <div className="dogBlock">
+                         <div className="eachDogNew">
+                             <form onSubmit={handleFormSubmit} >
+                                <div>
+                                 <label htmlFor="doggo">name: <input
+                                     type="text"
+                                     name="name"
+                                     autoComplete=""
+                                     value={dogFormState.name}
+                                     onChange={handleChange}
+                                 /></label>
+                                 
+                             
+                                 <label htmlFor="doggo">size: <input
+                                     type="text"
+                                     name="size"
+                                     autoComplete=""
+                                     value={dogFormState.size}
+                                     onChange={handleChange}
+                                 /></label>
+                                
+                                 <label htmlFor="doggo">age: <input
+                                     type="text"
+                                     name="age"
+                                     autoComplete=""
+                                     value={dogFormState.age}
+                                     onChange={handleChange}
+                                 /></label>
+                                 
+                                 <label htmlFor="doggo">breed: <input
+                                     type="text"
+                                     name="breed"
+                                     autoComplete=""
+                                     value={dogFormState.breed}
+                                     onChange={handleChange}
+                                 /></label>
+                                 
+                                 <label htmlFor="doggo">behavior: <input
+                                     type="text"
+                                     name="behavior"
+                                     autoComplete=""
+                                     value={dogFormState.behavior}
+                                     onChange={handleChange}
+                                 /></label>
+                              
+                                 <label htmlFor="doggo">temperament:<input
+                                     type="text"
+                                     name="temperament"
+                                     autoComplete=""
+                                     value={dogFormState.temperament}
+                                     onChange={handleChange}
+                                 /></label>
+                                 
+                                 <label htmlFor="doggo">picture: <input
+                                     type="file"
+                                     name="picture"
+                                     autoComplete=""
+                                     
+                                     onChange={handleChange}
+                                 /></label>
+                                </div>
+                                 <button className="submitNewDog" type="submit">
+                                     Add Dog
+                                         </button>
+
+                                {addDogError && <div className="error">{addDogError.toString()}</div>}
+
+
+                             </form>
+                         </div>
+                     </div> : ""}
                          { loading ? (<div>...Loading</div>
                          ) : (
                             
-                             user.doggos.map(doggos => (
+                             me.doggos.map(doggos => (
                                  <div key={doggos._id} className="dogBlock">
                                      
                                      <div className="eachDog">
@@ -248,16 +380,21 @@ const user = data?.user || [];*/}
                                              <p>Temperament: {doggos.temperament}</p>
                                              <p>Special Instructions:</p>
                                          </div>
-                                         <div className="dogPic"></div>
+
+                                         <div className="dogPic">
+
+                                             <img src={doggos.picture} alt={"picture of " + `${doggos.name}`}/>
+                                           </div>
                                      </div>
                                  </div>)))}
                 
-            </div>
-            
-        
-                </div>
-             </div> */}
-
+                            </div>
+                        </div>
+                    </div> 
+             <div>
+                 
+                 </div>
+                    
                  <div className = "bottomOrange"></div>
          </div>
      )
